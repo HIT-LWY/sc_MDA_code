01b36a236f8d199b1b2dd1c28f5888d8a2fb2e2d
HIT-LWY <2447698068@qq.com>
2024-10-23 21:17:58 +0800

    完成代码生成的后端

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import *
 from flask import Flask
-
+from flask_cors import *
+import modelConvert
+from codeGeneration import outputCode
 from modelConvert import writeFile, parsePUML, convert
 
 app = Flask(__name__)  # 初始化app
+CORS(app, resources=r'/*')
 
 
 @app.route('/')  # 建立路由
@@ -33,5 +36,25 @@ def convertModel():
     return result
 
 
+@app.route('/generateCode', methods=['POST'])
+@cross_origin()
+def generate():
+    content = request.json.get('pimCode')
+    Note = open('tmp_cim\cim.puml', mode='a')
+    Note.seek(0)
+    Note.truncate()
+    Note.write(content)
+    Note.close()
+    # 解析CIM
+    parsePUML()
+    convert()
+    outputCode()
+    with open('code_result/licencePSM.sol', 'r', encoding='utf-8') as file:
+        result = file.read()
+        return result
+
+
 if __name__ == '__main__':
-    app.run()  # 运行app
+    modelConvert.truncateCimDB()
+    modelConvert.truncatePimDB()
+    app.run(host='127.0.0.1', port=9014,)  # 运行app
